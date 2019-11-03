faf2011520c05295128a11f235347b6e414f3845
tsuyoshi-matsuura <tsuyoshi.matsuura@sigmaenergy.nl>
2019-11-03 08:53:42 +0100

    update

--- DataCleaning.R
+++ DataCleaning.R
@@ -1,5 +1,13 @@
 library(tidyverse)
 library(tidytext)
+library(textclean)
+
+# Read a file with apostrophe words
+appos <- read.csv("appos.txt",stringsAsFactors = FALSE)
+
+# Read a file with profanity words
+profanity <- readLines("profanity.txt")
+profanity <- tibble(word=profanity)
 
 # Function to read a random fraction of the input file
 # All numbers are replaced by blanks
@@ -10,29 +18,71 @@ readFile = function(filepath,fraction,seed) {
     close(con)
     set.seed(seed)
     lines <- sample(lines,length(lines)*fraction)
+    # Cast to lower case
+    lines <- tolower(lines)
+    # Replace words with appostrophe
+    lines <- mgsub(lines,appos$appos,appos$repl)
+    # Remove numbers
     lines <- gsub("\\b[0-9][0-9.,eE+-]*\\b","",lines)
+    # Remove all non letters and non spaces
+    lines <- gsub("[^a-zA-Z ]","",lines)
+    output <- tibble(line=1:length(lines),text=lines)
+    output
+}
+
+readFileCleanOld = function(filepath,fraction,seed) {
+    # Open as "rb" to avoid ^Z problem
+    con = file(filepath, "rb")
+    lines <- readLines(con,encoding = "UTF-8")
+    close(con)
+    set.seed(seed)
+    lines <- sample(lines,length(lines)*fraction)
+    # Cast to lower case and remove non relevant symbols
+    # Letters, spaces and apostrophes are retained
+    lines <- strip(lines)
+    # Replace contrations with their multi-word form, e,g I'll -> I will
+    lines <- replace_contraction(lines)
+    # Remove profanity
+    lines <- mgsub(lines,profanity,"")
+    output <- tibble(line=1:length(lines),text=lines)
+    output
+}
+
+readFileClean <-  function(filepath,fraction,seed) {
+    # Open as "rb" to avoid ^Z problem
+    con = file(filepath, "rb")
+    lines <- readLines(con,encoding = "UTF-8")
+    close(con)
+    set.seed(seed)
+    lines <- sample(lines,length(lines)*fraction)
+    # Cast to lower case
+    lines <- tolower(lines)
+    # Replace contrations with their multi-word form, e,g I'll -> I will
+    lines <- replace_contraction(lines)
+    # Remove profanity
+    lines <- mgsub(lines,profanity,"")
+    # Remove numbers
+    lines <- gsub("\\b[0-9][0-9.,eE+-]*\\b","",lines)
+    # Remove all non letters and non spaces
+    lines <- gsub("[^a-zA-Z ]","",lines)
     output <- tibble(line=1:length(lines),text=lines)
     output
 }
 
 # Read 10% of the blogs, news and twitter data
-blogs <- readFile("en_US/en_US.blogs.txt",0.1,124)
-news <- readFile("en_US/en_US.news.txt",0.1,124)
-twitter <- readFile("en_US/en_US.twitter.txt",0.1,124)
+blogs <- readFileClean("en_US/en_US.blogs.txt",0.1,124)
+news <- readFileClean("en_US/en_US.news.txt",0.1,124)
+twitter <- readFileClean("en_US/en_US.twitter.txt",0.1,124)
 
 # Combine the above into a single corpus
 # Mark the source of each of the data
 corpus <- bind_rows(list(blogs=blogs,news=news,twitter=twitter),.id="source")
 
-# Read a file with profanity words
-profanity <- readLines("profanity.txt")
-profanity <- tibble(word=profanity)
-
 # Tokenize into unigrams using unnest_token
-words <- corpus %>% unnest_tokens(word,text,token="words") # 10054741
+words <- corpus %>% unnest_tokens(word,text,token="words") # 10090397
 # Remove profanities and "snowball" stopwords
-words <- words %>% anti_join(profanity) # 10042330 
-words <- words %>% anti_join(stop_words[stop_words$lexicon=="snowball",]) # 5597926
+#words <- words %>% anti_join(profanity) # 9951709 
+words <- words %>% anti_join(stop_words[stop_words$lexicon=="snowball",]) # 5555541 
 
 # Determine term frequency for each source
 source_words <-  words %>% count(source,word,sort=TRUE)
@@ -54,14 +104,14 @@ source_words %>% ggplot(aes(tf,fill=source))+
 # Visualize the top ten words for each source
 pd_words <-  source_words %>% 
     group_by(source) %>% 
-    top_n(10, tf) %>% 
+    top_n(20, tf) %>% 
     ungroup() %>%
     arrange(source,desc(tf)) %>%
     mutate(order=row_number())
 
 pd_words %>% ggplot(aes(x=-order, y=tf, fill = source)) +
     geom_col(show.legend = FALSE) +
-    facet_wrap(~source, ncol=1,scales="free_y") +
+    facet_wrap(~source, ncol=3,scales="free_y") +
     labs(x = NULL, y = "term frequency") +
     scale_x_continuous(
         breaks = -pd_words$order,
@@ -71,7 +121,7 @@ pd_words %>% ggplot(aes(x=-order, y=tf, fill = source)) +
     coord_flip()
 
 # unique words in sample
-length(unique(source_words$word)) # 183224
+length(unique(source_words$word)) #  214980
 
 # Tokenize into bigrams
 bigrams <- corpus %>% unnest_tokens(bigram,text,token="ngrams", n=2)
@@ -82,8 +132,8 @@ bigrams <- corpus %>% unnest_tokens(bigram,text,token="ngrams", n=2)
 bigrams <- bigrams %>% separate(bigram, c("word1","word2"),sep=" ") %>%
     filter(!word1 %in% stop_words[stop_words$lexicon=="snowball",1]) %>%
     filter(!word2 %in% stop_words[stop_words$lexicon=="snowball",1]) %>%
-    filter(!word1 %in% profanity$word) %>%
-    filter(!word2 %in% profanity$word) %>%
+    #filter(!word1 %in% profanity$word) %>%
+    #filter(!word2 %in% profanity$word) %>%
     unite(bigram, word1, word2, sep=" ")
 
 # Determine term frequency for each source
@@ -96,7 +146,7 @@ source_bigrams <- source_bigrams %>%
     mutate(rank = row_number(), tf = n/total)
 source_bigrams
 
-# There are many words that only occur very rarely
+# There are many bigramss that only occur very rarely
 source_bigrams %>% ggplot(aes(tf,fill=source))+
     geom_histogram(show.legend=FALSE)+
     xlim(NA,0.0005) +
@@ -106,14 +156,14 @@ source_bigrams %>% ggplot(aes(tf,fill=source))+
 # Visualize the top ten bigramss for each source
 pd_bigrams <- source_bigrams %>% 
     group_by(source) %>% 
-    top_n(10, tf) %>% 
+    top_n(20, tf) %>% 
     ungroup() %>%
     arrange(source,desc(tf)) %>%
     mutate(order=row_number())
 
 pd_bigrams %>% ggplot(aes(x=-order, y=tf, fill = source)) +
     geom_col(show.legend = FALSE) +
-    facet_wrap(~source, ncol=1,scales="free_y") +
+    facet_wrap(~source, ncol=3,scales="free_y") +
     labs(x = NULL, y = "term frequency") +
     scale_x_continuous(
         breaks = -pd_bigrams$order,
