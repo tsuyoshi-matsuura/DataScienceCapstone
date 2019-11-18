2de6ac837f8d001fd3fcc9affb4acb4ef083efcb
tsuyoshi-matsuura <tsuyoshi.matsuura@sigmaenergy.nl>
2019-11-18 20:19:22 +0100

    Update

--- ShinyApp/ui.R
+++ ShinyApp/ui.R
@@ -9,7 +9,7 @@ shinyUI(navbarPage("Next Word Prediction",
                    theme = shinytheme("spacelab"),
                    
                    #Tab 1
-                   tabPanel(p(icon("wrench"), "Next word tool"),
+                   tabPanel(p(icon("wrench"), "Next word app"),
                             
                             textInput("user_input", "Please enter some text:", value =""),
 
@@ -20,21 +20,21 @@ shinyUI(navbarPage("Next Word Prediction",
                    ),
                    # Tab 2
                    tabPanel(p(icon("book"), "Documentation"),
-                            h3("Purpose of the tool"),
-                            h5("The purpose of the tool is to predict up to five words
-                               that could follow the text provided by the user."),
+                            h3("Purpose of the app"),
+                            h5("The purpose of the app is to predict up to five 'Next Word's
+                               based on the text provided by the user."),
                             tags$hr(),
                             h3("Usage"),
-                            h5("Provide some text in the designated input box. The tool will
-                               generate up to five words that could follow the text. If the user
+                            h5("Provide some text in the designated input box. The app will
+                               generate up to five 'Next Word's that could follow the text. If the user
                                continues typing text, the predicted words will be updated."),
                             tags$hr(),
                             h3("Technical background"),
-                            h5("The tool is based on a quad-gram language model with Kneser-Ney smoothing. 
-                               The tool was trained on 90% of the English blogs, news and twitter text
-                               that was provided by the course. To keep the size of the app manageable and
+                            h5("The app is based on a quad-gram language model with Kneser-Ney smoothing. 
+                               The app was trained on 90% of the provided English blogs, news and twitter text.
+                               To keep the size of the app manageable and
                                the response time reasonable the generated n-grams were pruned."),
-                            h5("The prediction accuracy is expected to be around 15% based on tests using the remaining 10%
+                            h5("The prediction accuracy is expected to be around 15% based on a test using the remaining 10%
                                of the blogs, news and twitter data.")
                    )
 ))
